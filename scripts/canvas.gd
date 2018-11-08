3fb852a701d2d2f45a4d2cee3dafab523829d478
Fju <flowi@outlook.com>
2018-11-08 19:13:31 +0100

    Created singleton for global variables & constants
    Tied up the structure and dependencies of the single scripts. A singleton script `tetrout.gd` is loaded which contains all useful constant and loads the basic block texture. Also created a "class" for Block objects that can be rendered just like normal Node2D's. This is useful for the "shoot" animation.

--- scripts/canvas.gd
+++ scripts/canvas.gd
@@ -3,28 +3,21 @@ extends Control
 #import classes
 const Block = preload("res://scripts/block.gd")
 
-const TEXTURE_FLAG = 0 # prevent interpolation filter
+var TETRIS_CANVAS_WIDTH = tetrout.TETRIS_BLOCK_SIZE * tetrout.TETRIS_COLUMNS
+var TETRIS_CANVAS_HEIGHT = tetrout.TETRIS_BLOCK_SIZE * tetrout.TETRIS_ROWS
 
-var columns = 16
-var rows = 12
-
-var box_height = rows * Block.SIZE
-var box_width = columns * Block.SIZE
-
-
-var block_texture
 var area = []
 
-func _init(r=12, c=16):
-	rows = r
-	columns = c
+var redraw = true
+
+func _init():
 	clear_area()
 
 func _ready():
-	set_size(Vector2(box_width, box_height))
+	set_size(Vector2(TETRIS_CANVAS_WIDTH, TETRIS_CANVAS_HEIGHT))
 	
-	var bl = Block.new(Block.TYPES.CYAN)
-	var bt = Block.new(Block.TYPES.RED)
+	var bl = Block.new(tetrout.TETRIS_BLOCK_TYPES.RED)
+	var bt = Block.new(tetrout.TETRIS_BLOCK_TYPES.YELLOW)
 	#bl.rotate(1)
 	
 	add_block(bl, Vector2(0, 0))
@@ -33,45 +26,38 @@ func _ready():
 	add_block(bl, Vector2(3, 0))
 	
 	add_block(bt, Vector2(5, 1))
-	load_textures()
 
 func clear_area():
-	for column in range(columns):
+	for column in range(tetrout.TETRIS_COLUMNS):
 		area.append([])
-		for row in range(rows):
-			area[column].append(Block.TYPES.EMPTY)
-
-func load_textures():
-	block_texture = ImageTexture.new()
-	var img = Image.new()
-	img.load("res://assets/blocks/single_block.png")
-	img.resize(Block.SIZE, Block.SIZE, 0)
-	block_texture.create_from_image(img, TEXTURE_FLAG)
+		for row in range(tetrout.TETRIS_ROWS):
+			area[column].append(tetrout.TETRIS_BLOCK_TYPES.EMPTY)
 
 func add_block(block, pos):
-	print(block.width)
 	for y in range(block.height):
 		for x in range(block.width):
 			if block.matrix[y][x] == 1:
 				area[pos.x+x][pos.y+y] = block.type
 
+# TODO
+#func get_global_pos_of_block(block_pos):
+	
 
-var needs_update = true
 func _draw():
-	if !needs_update:
+	if !redraw:
 		return
 	
-	# draw warning line
-	draw_rect(Rect2(Vector2(0, 0), Vector2(box_width, box_height)), Color(1, 1, 1, 0.15), true)
+	redraw = false
 	
-	needs_update = false
-	for column in range(columns):
-		var y = Block.SIZE * column
-		for row in range(rows):
-			var x = box_width - Block.SIZE * (row + 1)
+	draw_rect(Rect2(Vector2(0, 0), Vector2(TETRIS_CANVAS_WIDTH, TETRIS_CANVAS_HEIGHT)), Color(1, 1, 1, 0.15), true)
+	
+	for column in range(tetrout.TETRIS_COLUMNS):
+		var y = tetrout.TETRIS_BLOCK_SIZE * column
+		for row in range(tetrout.TETRIS_ROWS):
+			var x = TETRIS_CANVAS_WIDTH - tetrout.TETRIS_BLOCK_SIZE * (row + 1)
 			var type = area[column][row]
 			# draw blocks
-			draw_texture(block_texture, Vector2(x, y), Block.get_color(type))
+			draw_texture(tetrout.block_texture, Vector2(x, y), tetrout.get_block_color(type))
 	
 	
 	
