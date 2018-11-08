98487d09187ab2f02ef2895fea2d9df7a4fd7448
Max Schönleben <max.schoenleben@gmail.com>
2018-11-09 00:17:50 +0100

    ROTATION!!!
    It took a little bit to figure it out, what I need to do in order to rotate the ship in the direction it's moving, but know it works and its pretty simple. (In the comment of the code you can see my former try to make it work)

--- scripts/AnimatedSprite.gd
+++ scripts/AnimatedSprite.gd
@@ -18,12 +18,16 @@ func _process(delta):
     var velocity = Vector2() # The player's movement vector.
     if Input.is_action_pressed("ui_down"):
         velocity.y += 1
-        #rotation_degrees.set_rotation_degrees(115)
+        rotation_degrees = 115
+        #rotation_degrees.set_rotation_degrees(115.0)
     if Input.is_action_pressed("ui_up"):
         velocity.y -= 1
-        #rotation_degrees.set_rotation_degrees(65)
+        rotation_degrees = 65
+        #rotation_degrees.set_rotation_degrees(65.0)
     if velocity.length() > 0:
         velocity = velocity.normalized() * speed
+    else:
+	    rotation_degrees = 90
     position += velocity * delta
     position.x = clamp(position.x, 0, screensize.x)
     position.y = clamp(position.y, 0, screensize.y)
