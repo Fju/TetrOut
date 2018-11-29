d76fec6f05d62415c683d1b8564a1241979ede67
Fju <flowi@outlook.com>
2018-11-29 13:24:17 +0100

    Added particle effect to menu

--- scripts/block_particles.gd
+++ scripts/block_particles.gd
@@ -1,20 +1,13 @@
-extends Control
+extends CanvasLayer
 
 onready var viewport = get_viewport()
 
 func _ready():
 	viewport.connect('size_changed', self, '_on_viewport_size_changed')
-
-func _on_Start_pressed():
-	get_tree().change_scene('res://scenes/main.tscn')
-	
-
-func _on_Exit_pressed():
-	get_tree().quit()
-
+	_on_viewport_size_changed()
 
 func _on_viewport_size_changed():
 	var window_size = viewport.get_size_override()
 	
 	# adapt background size, so that it covers the whole screen
-	$BackgroundLayer/Background.region_rect.size = window_size
+	$Particles2D.set_position(window_size)
